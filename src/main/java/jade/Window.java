ac827ca5d3e99379b1b22cc2f43e5ff45cb05a33
Lucas <lucas13dealmeidamoraes@gmail.com>
2024-07-16 14:50:23 -0300

    Component abstract class inherited by GameObject class. RenderBatch and Renderer

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -34,10 +34,12 @@ public class Window {
             case 0:
                 currentScene = new LevelEditorScene();
                 currentScene.init();
+                currentScene.start();
                 break;
             case 1:
                 currentScene = new LevelScene();
                 currentScene.init();
+                currentScene.start();
                 break;
             default:
                 assert false: "Unknown scene '" + newScene + "'";
@@ -51,6 +53,10 @@ public class Window {
         }
 
         return Window.window;
+    }
+
+    public static Scene getScene() {
+        return get().currentScene;
     }
 
     public void run() {
